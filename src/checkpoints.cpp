d561e2534ae8ead01e2d34deb42b5e459af2c8fc
ozziecoin <ozziecoin@gmail.com>
2014-04-04 21:34:54 +0800

    Checkpoint syntax fix on line 38

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -35,7 +35,7 @@ namespace Checkpoints
     // + Contains no strange transactions
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        (  NULL, NULL))    
+        (  NULL, NULL)    
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,

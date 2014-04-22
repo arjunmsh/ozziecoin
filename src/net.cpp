5d268330bb5afb44f6dce81a7c613605342876f8
ozziecoin <ozziecoin@gmail.com>
2014-04-22 14:56:48 +0800

    dns seed change

--- src/net.cpp
+++ src/net.cpp
@@ -1192,16 +1192,17 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"", "107.170.235.189"},
-    {"", "128.199.214.79"},
     {"ozziecoin.com", "seed1.ozziecoin.com"},
+    {NULL, NULL},
+    {NULL, NULL},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
     {"ozziecoin.com", "testseed1.ozziecoin.com"},
-    {"", "107.170.235.189"},
-    {"", "128.199.214.79"}
+    {NULL, NULL},
+    {NULL, NULL}
+	
 };
 
 void ThreadDNSAddressSeed()
